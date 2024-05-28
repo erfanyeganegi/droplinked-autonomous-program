ee58a0fbff9099ab98aaf65ad0794f2343c4c290
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2024-05-29 03:18:06 +0330

    chore: create state module

--- programs/droplinked-autonomous-program/src/lib.rs
+++ programs/droplinked-autonomous-program/src/lib.rs
@@ -2,6 +2,10 @@ use anchor_lang::prelude::*;
 
 declare_id!("Du3qfvSDkDq92p9dYV6XhRPW6TRRtHbGnH3tiNfJjYWp");
 
+pub mod state;
+
+use state::*;
+
 #[program]
 pub mod droplinked_autonomous_program {
     use super::*;
