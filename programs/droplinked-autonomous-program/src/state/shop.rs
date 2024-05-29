588c1e3c7535a7419370b694fa03da2c8116e6a7
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2024-05-29 05:04:31 +0330

    feat: implement IsState trait for Shop

--- programs/droplinked-autonomous-program/src/state/shop.rs
+++ programs/droplinked-autonomous-program/src/state/shop.rs
@@ -1,4 +1,8 @@
+use std::mem::size_of;
+
 use anchor_lang::prelude::*;
+
+use crate::IsState;
 
 #[account]
 pub struct Shop {
@@ -8,4 +12,24 @@ pub struct Shop {
     pub updated_at: i64,
     pub is_active: bool,
     pub is_verified: bool,
+}
+
+pub struct ShopSpaceParams {
+    metadata: String,
+}
+
+impl IsState<ShopSpaceParams> for Shop {
+    const PREFIX: &'static str = "shop";
+
+    fn space(params: ShopSpaceParams) -> usize {
+        let ShopSpaceParams { metadata } = params;
+
+        8 /* discriminator */
+      + size_of::<Pubkey>() /* owner */
+      + (4 + metadata.as_bytes().len()) /* metadata */
+      + size_of::<i64>() /* created_at */
+      + size_of::<i64>() /* updated_at */
+      + size_of::<bool>() /* is_active */
+      + size_of::<bool>() /* is_verified */
+    }
 }
