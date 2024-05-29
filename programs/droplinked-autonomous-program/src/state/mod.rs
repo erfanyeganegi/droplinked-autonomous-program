6c381acc176a353ec0984dca6be3efd89f6ab4d4
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2024-05-29 05:04:05 +0330

    feat: define IsState trait

--- programs/droplinked-autonomous-program/src/state/mod.rs
+++ programs/droplinked-autonomous-program/src/state/mod.rs
@@ -1,3 +1,13 @@
 mod shop;
 
 pub use shop::*;
+
+pub trait IsState<T> {
+    const PREFIX: &'static str;
+
+    fn prefix() -> &'static str {
+        Self::PREFIX
+    }
+
+    fn space(params: T) -> usize;
+}
